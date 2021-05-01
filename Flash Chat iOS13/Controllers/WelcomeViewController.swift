b7d4761fb6c043d42c9c5ad0655d7e7993684259
Lucas Brito <lucasbrito_9@hotmail.com>
2021-05-01 20:46:37 -0300

    Created animation in titleLabel, added gitignore and cocoapods.

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,18 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        setupAnimationTitle()
     }
     
-
+    func setupAnimationTitle() {
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
 }
